2774aafc6da83b60d0d454518a21f89d08c7506d
unknown <bvadva111@gmail.com>
2021-10-27 15:02:25 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,21 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 10;
+		boolean c =a > b;
+		c = a < b;
+		System.out.println(a + b );
+		System.out.println(a - b );
+		System.out.println(a * b );
+		System.out.println(a / b );
+		System.out.println(a > b ? "a lon hon b" : "b lon hon a" );
+		System.out.println(a < b ? "a be hon b" : "b be hon a" );
+		System.out.println(a >= b ? " a lon hon hoac bang b" : "b lon hon hoac bang a" );
+		System.out.println(a <= b ? "a be hoac bang b" : "b lon hon hoac bang a" );
+	}
+
+}
